5f9afadd55de61aa5f240844ef1e1e42ed408ae4
Anzhelika <293852647@mail.ru>
2022-01-31 20:03:45 +0300

    lab-1 done

--- js/script.js
+++ js/script.js
@@ -24,3 +24,42 @@ const movieDB = {
     ]
 };
 
+//1
+const adv = document.querySelectorAll('.promo__adv img');
+/* adv.remove(); *///не сработает - у нас коллекция(псевдо-массив)
+
+adv.forEach(item => {
+    item.remove();
+});//перебрали массив и удалили каждый элемент
+
+//обычная функция - одноразовая, без имени (тоже самое)
+/* adv.forEach(function (item) {
+    item.remove();
+}); */
+
+//2
+const poster = document.querySelector('.promo__bg'),
+    genre = poster.querySelector('.promo__genre');
+
+genre.textContent = "Драмма";//поменяли жанр
+
+//3
+poster.style.backgroundImage = 'url("img/bg.jpg")'; //поменяли фон
+
+//4
+const movieList = document.querySelector('.promo__interactive-list');
+
+movieList.innerHTML = "";//удалили по селектору
+
+movieDB.movies.sort();//обратились к базе (объекту - свойство-movies - массив) и отсортировали
+
+/* console.log(poster.innerHTML); */
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+    <li class="promo__interactive-item">${i + 1} ${film}
+        <div class="delete"></div>
+    </li>
+    `;
+});
+
